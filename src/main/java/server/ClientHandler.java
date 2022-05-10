48c6baf904812fdeedc7787c0e8b2d2f72d84abc
cphillip021 <cphillip021@student.wethinkcode.co.za>
2022-05-10 12:02:59 +0200

    server client updates

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -1,2 +1,25 @@
-package server;public class ClientHandler {
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClientHandler extends Thread{
+    public static final int PORT = 5000;
+    private Socket socket;
+
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader (socket.getInputStream()));
+             final PrintStream out = new PrintStream(socket.getOutputStream())) {
+
+        } catch(IOException ex) {
+            System.out.println ("Shutting down server : " + ex);
+        }
+    }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,2 +1,24 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket s = new ServerSocket( ClientHandler.PORT);
+
+        while (true) {
+            try {
+                Socket socket = s.accept();
+                System.out.println("Connection: " + socket);
+                Thread r = new ClientHandler(socket);
+//                Thread task = new Thread(r);
+                r.start();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
