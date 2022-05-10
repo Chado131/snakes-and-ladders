b1181be68d14318726d61c9fab588b9a56422d6d
lvan-wyk021.cpt <lvan-wyk021@student.wethinkcode.co.za>
2022-05-10 13:16:21 +0200

    Added a fully functioning Main class to run the game offline.

--- src/main/java/game/Die.java
+++ src/main/java/game/Die.java
@@ -4,13 +4,21 @@ import java.util.Random;
 
 public class Die {
     Random random;
+    private int face;
 
     public Die() {
         this.random = new Random();
+        this.face = 0;
     }
 
     public int roll() {
-        return random.nextInt(6) + 1;
+        int face = random.nextInt(6) + 1;
+        this.face = face;
+        return face;
+    }
+
+    public int getFace() {
+        return this.face;
     }
 
 }
--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,5 +1,7 @@
 
 package game;
+import server.SwingGUI;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,15 +10,34 @@ import java.util.Scanner;
 public class Main {
     static ArrayList<Player> players = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
+    static SwingGUI gui = new SwingGUI(new Board(10, 10, 10));
 
     public static void main(String[] args) {
         Board board = new Board(10, 10, 10);
         System.out.println("~~~~~~~~~ SNAKES AND LADDERS ~~~~~~~~~");
+        System.out.println("");
 
         int numPlayers = Integer.parseInt(getInput("How many players for this game?"));
         setUpPlayers(numPlayers);
 
         while (!gameOver(players)) {
+
+            for (int i = 0; i < numPlayers; i++) {
+                System.out.println("");
+                rollDie("Player " + (i + 1) + ", press ENTER to roll your die.");
+                Player player = players.get(i);
+                Die die = player.getDie();
+                board.takeTurn(player);
+                System.out.println(player.getName() + ", you have rolled a " + die.getFace() + ".");
+                System.out.println(player.getName() + ", you are now on cell " + player.getCell().getCellNumber() + ".");
+
+                if (die.getFace() == 6) {
+                    rollDie("Player " + (i + 1) + ", press ENTER to roll your die.");
+                    board.takeTurn(player);
+                    System.out.println(player.getName() + ", you have rolled a " + die.getFace() + ".");
+                    System.out.println(player.getName() + ", you are now on cell " + player.getCell().getCellNumber() + ".");
+                }
+            }
 
         }
 
@@ -50,6 +71,11 @@ public class Main {
             input = scanner.nextLine();
         } while (input.isBlank());
         return input;
+    }
+
+    private static void rollDie(String prompt) {
+        System.out.println(prompt);
+        String input = scanner.nextLine();
     }
 
 
