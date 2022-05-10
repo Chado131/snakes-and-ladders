b09a9cb727248f6063d7cfecdbd1520d1390ad64
mmoss <mmoss020@student.wethinkcode.co.za>
2022-05-10 12:58:04 +0200

    oh lawdy

--- src/main/java/server/Client.java
+++ src/main/java/server/Client.java
@@ -12,18 +12,21 @@ import java.util.ArrayList;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        String ip = args[0];
-        String port = args[1];
-
-        try (Socket socket = new Socket(ip, Integer.parseInt(port));
-             PrintStream out = new PrintStream(socket.getOutputStream());
-             BufferedReader in = new BufferedReader(new InputStreamReader (socket.getInputStream()))
-        ) {
-            SwingGUI gui = new SwingGUI (new Board (10, 10, 10));
-            ArrayList<Player> players = new ArrayList<> ();
-            gui.showOutput (players);
-        } catch (IOException e) {
-            e.printStackTrace ();
-        }
+        SwingGUI gui = new SwingGUI (new Board (10, 10, 10));
+        ArrayList<Player> players = new ArrayList<> ();
+        gui.showOutput (players);
+//        String ip = args[0];
+//        String port = args[1];
+//
+//        try (Socket socket = new Socket(ip, Integer.parseInt(port));
+//             PrintStream out = new PrintStream(socket.getOutputStream());
+//             BufferedReader in = new BufferedReader(new InputStreamReader (socket.getInputStream()))
+//        ) {
+//            SwingGUI gui = new SwingGUI (new Board (10, 10, 10));
+//            ArrayList<Player> players = new ArrayList<> ();
+//            gui.showOutput (players);
+//        } catch (IOException e) {
+//            e.printStackTrace ();
+//        }
     }
 }
