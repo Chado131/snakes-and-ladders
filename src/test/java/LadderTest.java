9748369078e8adc29276a85830e2327b30ca3892
mmoss <mmoss020@student.wethinkcode.co.za>
2022-05-10 09:57:21 +0200

    changes to tests, refactored player

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -3,10 +3,18 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+    @Test
+    public void testConstructor(){
+        Movement m = new Ladder(new Cell(5), new Cell(20));
+        assertEquals(new Cell(5), m.getTop());
+        assertEquals(new Cell(20), m.getBottom());
+    }
+
     @Test
     public void testMove(){
         Player player = new Player("Tester");
         new Ladder(new Cell(40), new Cell(10)).move(player);
-        assertEquals(new Cell(40), player.getCurrentCell());
+        assertEquals(new Cell(40), player.getCell());
     }
 }
--- src/test/java/MovementTest.java
+++ src/test/java/MovementTest.java
@@ -3,10 +3,5 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovementTest {
-    @Test
-    public void testConstructor(){
-        Movement m = new Movement(new Cell(5), new Cell(20));
-        assertEquals(new Cell(5), m.getTop());
-        assertEquals(new Cell(20), m.getBottom());
-    }
+
 }
