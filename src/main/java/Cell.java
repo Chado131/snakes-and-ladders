8f6c1962549c675e1cb839e52d91ffc563f7a895
cphillip021 <cphillip021@student.wethinkcode.co.za>
2022-05-10 09:47:58 +0200

    cell class and player class

---
+++ src/main/java/Cell.java
@@ -0,0 +1,26 @@
+public class Cell {
+    private int cellNumber;
+
+    public Cell(int cell) {
+        this.cellNumber = cell;
+    }
+
+    public int getCellNumber() {
+        return cellNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell position = (Cell) o;
+
+        if (cellNumber != position.cellNumber) return true;
+    }
+}
