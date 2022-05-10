461118a5d4e62c8bd80110fe07b9ed5a4c7af894
lvan-wyk021.cpt <lvan-wyk021@student.wethinkcode.co.za>
2022-05-10 09:59:02 +0200

    Die now return an int between 1 & 6

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,10 +1,14 @@
+import java.util.Random;
+
 public class Die {
+    Random random;
 
     public Die() {
+        this.random = new Random();
 
     }
 
     public int roll() {
-        return 5;
+        return random.nextInt(6) + 1;
     }
 }
