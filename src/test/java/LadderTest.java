ac16832397f471b3d1269abd0c612f5ae51db616
mmoss <mmoss020@student.wethinkcode.co.za>
2022-05-10 10:01:31 +0200

    tests sorted

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,18 +1,18 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
 
     @Test
-    public void testConstructor(){
+    void testConstructor(){
         Movement m = new Ladder(new Cell(5), new Cell(20));
         assertEquals(new Cell(5), m.getTop());
         assertEquals(new Cell(20), m.getBottom());
     }
 
     @Test
-    public void testMove(){
+    void testMove(){
         Player player = new Player("Tester");
         new Ladder(new Cell(40), new Cell(10)).move(player);
         assertEquals(new Cell(40), player.getCell());
